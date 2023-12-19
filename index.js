aba78ed8dc1d257b6b652e78a3ceb8c04274b48b
Kartikeya Gupta <g.kartikeya123@gmail.com>
2023-12-19 21:26:37 +0530

    github data added

--- index.js
+++ index.js
@@ -4,6 +4,41 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const githubData = {
+  "login": "Kartikeya-g",
+  "id": 81621960,
+  "node_id": "MDQ6VXNlcjgxNjIxOTYw",
+  "avatar_url": "https://avatars.githubusercontent.com/u/81621960?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/Kartikeya-g",
+  "html_url": "https://github.com/Kartikeya-g",
+  "followers_url": "https://api.github.com/users/Kartikeya-g/followers",
+  "following_url": "https://api.github.com/users/Kartikeya-g/following{/other_user}",
+  "gists_url": "https://api.github.com/users/Kartikeya-g/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/Kartikeya-g/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/Kartikeya-g/subscriptions",
+  "organizations_url": "https://api.github.com/users/Kartikeya-g/orgs",
+  "repos_url": "https://api.github.com/users/Kartikeya-g/repos",
+  "events_url": "https://api.github.com/users/Kartikeya-g/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/Kartikeya-g/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": null,
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 4,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2021-03-30T10:16:39Z",
+  "updated_at": "2023-10-15T13:23:22Z"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -14,6 +49,10 @@ app.get('/login', (req, res) => {
 
 app.get('/twitter', (req, res) => {
   res.send('Kartikcallingkartik')
+})
+
+app.get('/githubData', (req, res) => {
+  res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
